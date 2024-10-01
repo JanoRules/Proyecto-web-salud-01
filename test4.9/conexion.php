a9913931e47ea7e05ad9f853bcc092dffc247e70
Ariel Andrés Díaz Lineros <adiaz2024@alu.uct.cl>
2024-10-01 13:09:38 -0300

    Add files via upload

---
+++ test4.9/conexion.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Cambia esto si usas otro usuario
+$password = ""; // contraseña de root SQL
+$database = "base_datos";
+
+try {
+    $conn = new mysqli($servername, $username, $password, $database);
+    
+    // Verifica la conexión
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+} catch (mysqli_sql_exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
