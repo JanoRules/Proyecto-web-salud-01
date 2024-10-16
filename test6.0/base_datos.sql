b924c2c8d55f34e984c23eaf9afb4ea714b283ee
Ariel Andrés Díaz Lineros <adiaz2024@alu.uct.cl>
2024-10-15 23:31:01 -0300

    Add files via upload

--- test6.0/base_datos.sql
+++ test6.0/base_datos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2024 a las 20:39:34
+-- Tiempo de generación: 16-10-2024 a las 04:13:27
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,25 +24,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `agendar_hora`
+-- Estructura de tabla para la tabla `busquedas_servicios`
 --
 
-CREATE TABLE `agendar_hora` (
+CREATE TABLE `busquedas_servicios` (
   `id` int(11) NOT NULL,
-  `id_centro` int(11) DEFAULT NULL,
-  `fecha` date DEFAULT NULL,
-  `hora` time DEFAULT NULL
+  `servicio_id` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `agendar_hora`
---
-
-INSERT INTO `agendar_hora` (`id`, `id_centro`, `fecha`, `hora`) VALUES
-(11, 2, '2024-10-25', '18:22:00'),
-(12, 2, '2024-10-24', '19:30:00'),
-(13, 2, '2024-10-25', '19:30:00'),
-(14, 2, '2024-10-25', '22:30:00');
 
 -- --------------------------------------------------------
 
@@ -107,6 +97,18 @@ INSERT INTO `contacto` (`id`, `tipo_queja`, `region`, `descripcion`, `fecha`) VA
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `favoritos`
+--
+
+CREATE TABLE `favoritos` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `centro_salud_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `login`
 --
 
@@ -114,21 +116,24 @@ CREATE TABLE `login` (
   `id` int(11) NOT NULL,
   `rut` varchar(12) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `intentos_fallidos` int(11) DEFAULT 0,
+  `tiempo_bloqueo` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `login`
 --
 
-INSERT INTO `login` (`id`, `rut`, `username`, `password`) VALUES
-(1, '22.016.050-5', 'nicolas.fuentes2024', '$2y$10$kW9OsyZwf3jssb142i3GVeUlHFGcl/RYsmKtTFKIRMxMfuCUevIXu'),
-(2, '132006005', 'vegetta777', '$2y$10$AlvYFpa0vRDyY1sa/NYHOukiKwvK2OX.05QMU7RsUKAf0IUMaMahu'),
-(4, '145008005', 'willyrex', '$2y$10$dde8O0RO9ySTTOm0vALyHescmULIUIxKY.nLv0gIUb/56o5/04FKC'),
-(5, '125135405', 'kylianmbappe7', '$2y$10$uWPsUhBajAsXwa0JIdywxO4lhDMRrcG66VRgxibDG9yny06aSDnsm'),
-(6, '214445552', 'Maxpayne', '$2y$10$RCgZ8m56yaSV1.skvvYDmugfSrQ212beCfVplYNuLv0JKnoveHTWO'),
-(7, '19.800.695-1', 'nfuentes', '$2y$10$GBFHkJCGd5.fKJJ2AaOQleVdPJw3BhrBJgAd.wE/mUrk7g9o6H5Ou'),
-(8, '200806008', 'Montapuercos', '$2y$10$Mh1Q0ywx/srLi88P3lGFaORidWiWJkGi3cyusTQq1j7KxQehR3cKS');
+INSERT INTO `login` (`id`, `rut`, `username`, `password`, `intentos_fallidos`, `tiempo_bloqueo`) VALUES
+(1, '22.016.050-5', 'nicolas.fuentes2024', '$2y$10$kW9OsyZwf3jssb142i3GVeUlHFGcl/RYsmKtTFKIRMxMfuCUevIXu', 0, NULL),
+(2, '132006005', 'vegetta777', '$2y$10$AlvYFpa0vRDyY1sa/NYHOukiKwvK2OX.05QMU7RsUKAf0IUMaMahu', 0, NULL),
+(4, '145008005', 'willyrex', '$2y$10$dde8O0RO9ySTTOm0vALyHescmULIUIxKY.nLv0gIUb/56o5/04FKC', 0, NULL),
+(5, '125135405', 'kylianmbappe7', '$2y$10$uWPsUhBajAsXwa0JIdywxO4lhDMRrcG66VRgxibDG9yny06aSDnsm', 0, NULL),
+(6, '214445552', 'Maxpayne', '$2y$10$RCgZ8m56yaSV1.skvvYDmugfSrQ212beCfVplYNuLv0JKnoveHTWO', 0, NULL),
+(7, '19.800.695-1', 'nfuentes', '$2y$10$GBFHkJCGd5.fKJJ2AaOQleVdPJw3BhrBJgAd.wE/mUrk7g9o6H5Ou', 0, NULL),
+(8, '200806008', 'Montapuercos', '$2y$10$Mh1Q0ywx/srLi88P3lGFaORidWiWJkGi3cyusTQq1j7KxQehR3cKS', 0, NULL),
+(9, '345748374', 'Slayer', '$2y$10$j/XaHWUJxZ1tocDTRGtXCO59ACQezoGHIhnJtm91Vm5ZMenX6CV2a', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -157,44 +162,53 @@ INSERT INTO `registro` (`id`, `rut`, `nombre`, `apellidos`, `correo`, `fechaNaci
 (6, '125135405', 'Kylian ', 'Mbappe', 'kylianmbappe7@gmail.com', '2024-09-02', 'Puente alto'),
 (7, '214445552', 'Max', 'Payne', 'max@gmail.com', '2024-08-29', 'Puente alto'),
 (8, '19.800.695-1', 'Nicolás', 'Fuentes Calfullán', 'olaola@gmail.com', '2006-01-08', 'Temuco'),
-(9, '200806008', 'Monta', 'Puercos', 'figfuj5fg615@gfdfd.com', '2000-06-02', 'Temuco');
+(9, '200806008', 'Monta', 'Puercos', 'figfuj5fg615@gfdfd.com', '2000-06-02', 'Temuco'),
+(10, '345748374', 'Ariel', 'Diaz', 'Si@gmail.com', '2024-10-04', 'Temuco');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `reseñas`
 --
 
-CREATE TABLE `usuarios` (
-  `rut` char(10) NOT NULL,
-  `nombre` char(20) DEFAULT NULL,
-  `password` char(20) DEFAULT NULL,
-  `correo` char(30) DEFAULT NULL,
-  `ubicacion` char(30) DEFAULT NULL,
-  `estado_salud` char(30) DEFAULT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `fechaNacimiento` date DEFAULT NULL
+CREATE TABLE `reseñas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `producto` varchar(100) NOT NULL,
+  `calificacion` int(11) NOT NULL CHECK (`calificacion` >= 1 and `calificacion` <= 5),
+  `reseña` text NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Volcado de datos para la tabla `reseñas`
 --
 
-INSERT INTO `usuarios` (`rut`, `nombre`, `password`, `correo`, `ubicacion`, `estado_salud`, `apellidos`, `username`, `fechaNacimiento`) VALUES
-('227584938', 'sfddsfds', '$2y$10$pjpW4WGlL6.Eh', 'waza50@gmail.com', 'asasa', NULL, 'sdfsdfsd', 'asasa', '2024-09-20'),
-('333333333', 'dadssa', '$2y$10$0aclx2vUO47NZ', 'emeraldchaos30@gmail.com', 'Mexico', NULL, 'sadasdsad', 'afadssadasd', '2024-09-07');
+INSERT INTO `reseñas` (`id`, `nombre`, `producto`, `calificacion`, `reseña`, `fecha`) VALUES
+(1, 'Slayer', 'dfdfdfdf', 3, 'dfsdfs', '2024-10-16 00:57:11');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `agendar_hora`
+-- Indices de la tabla `busquedas_servicios`
 --
-ALTER TABLE `agendar_hora`
+ALTER TABLE `busquedas_servicios`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_centro` (`id_centro`);
+  ADD KEY `servicio_id` (`servicio_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `centros_salud`
@@ -207,6 +221,14 @@ ALTER TABLE `centros_salud`
 --
 ALTER TABLE `contacto`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`,`centro_salud_id`),
+  ADD KEY `centro_salud_id` (`centro_salud_id`);
 
 --
 -- Indices de la tabla `login`
@@ -224,20 +246,26 @@ ALTER TABLE `registro`
   ADD UNIQUE KEY `rut` (`rut`);
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla `reseñas`
 --
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`rut`);
+ALTER TABLE `reseñas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `agendar_hora`
+-- AUTO_INCREMENT de la tabla `busquedas_servicios`
 --
-ALTER TABLE `agendar_hora`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+ALTER TABLE `busquedas_servicios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `centros_salud`
@@ -252,26 +280,52 @@ ALTER TABLE `contacto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `registro`
 --
 ALTER TABLE `registro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `reseñas`
+--
+ALTER TABLE `reseñas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `agendar_hora`
+-- Filtros para la tabla `busquedas_servicios`
 --
-ALTER TABLE `agendar_hora`
-  ADD CONSTRAINT `agendar_hora_ibfk_1` FOREIGN KEY (`id_centro`) REFERENCES `centros_salud` (`id`);
+ALTER TABLE `busquedas_servicios`
+  ADD CONSTRAINT `busquedas_servicios_ibfk_1` FOREIGN KEY (`servicio_id`) REFERENCES `servicios` (`id`),
+  ADD CONSTRAINT `busquedas_servicios_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `login` (`id`);
+
+--
+-- Filtros para la tabla `favoritos`
+--
+ALTER TABLE `favoritos`
+  ADD CONSTRAINT `favoritos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login` (`id`),
+  ADD CONSTRAINT `favoritos_ibfk_2` FOREIGN KEY (`centro_salud_id`) REFERENCES `centros_salud` (`id`);
 
 --
 -- Filtros para la tabla `login`
